4475cd45325c8151e47441b55d7469c39870b855
mrvic <mrvic.predrag@gmail.com>
2020-01-22 19:18:02 +0100

    dohvaća randomno id iz tablice zupanijas

--- app/Zupanija.php
+++ app/Zupanija.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Zupanija extends Model
 {
-     public function mjestas()
+     public function mjestos()
     {
         return $this->hasMany('App\Mjesto');
     }
--- database/factories/MjestoFactory.php
+++ database/factories/MjestoFactory.php
@@ -9,9 +9,9 @@ $factory->define(Mjesto::class, function (Faker $faker) {
     return [
         'pbr' => $faker->unique()->numberBetween(10000, 59999),
         'naziv'=>$faker->unique()->city(),
+        // 'zupanija_id'=>$faker->numberBetween(1, 21),
         //jednostavniji ali ne i najbolji pristup
         //TODO dohvati stvarne id od zupanija
-        'zupanija_id'=>$faker->numberBetween(1, 21),
-        
+        'zupanija_id'=>App\Zupanija::all()->random(1)[0]->id,
     ];
 });
